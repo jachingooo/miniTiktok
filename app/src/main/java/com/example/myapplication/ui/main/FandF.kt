d2e8791145bc198736303b40ca0d7cbcf6cf8153
HC <1531176958@qq.com>
2022-08-07 14:39:45 +0800

    person ui

---
+++ app/src/main/java/com/example/myapplication/ui/main/FandF.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FandF : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fand_f)
+    }
+}
