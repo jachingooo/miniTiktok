505388a78129a139c3729ecac29c6f718a65ffc2
HC <1531176958@qq.com>
2022-08-08 20:55:14 +0800

    rewrite-personui

---
+++ app/src/main/java/com/qxy/minitiktok/video.kt
@@ -0,0 +1,11 @@
+package com.qxy.minitiktok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//每个具体的视频页
+class video : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
