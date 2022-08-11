dfca75bb1a8be758f4ab57dda8d5881e2b8326cc
JFsama <1362613241@qq.com>
2022-08-11 22:05:18 +0800

    签名

---
+++ miniTiktok2/app/src/main/java/com/qxy/miniTiktok/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.qxy.miniTiktok
+
+import android.app.Application
+import com.lzy.okgo.OkGo
+
+
+/**
+ * @description Application基类-初始化网络请求框架等
+ * @time:
+ * @author:
+ */
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //初始化OkGo
+        OkGo.getInstance().init(this)
+    }
+}
